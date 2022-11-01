c733ffcb14994b5b5e2a132eea74ed1c47dacbf0
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-11-01 13:26:24 +0900

    秘密鍵を分散してシェアを作成し、署名する部分まで実装できました。

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # SecretSharingRepo
 秘密分散スクリプト実装用のリポジトリです。
 
-
+## 注意事項！！
+このリポジトリで使用している秘密鍵は全て開発用！  
+絶対に本番環境では使用しないこと！
 
 ### Typescriptファイルの動かし方
 
@@ -9,8 +11,30 @@
 npm run start
 ```
 
+### 秘密鍵からSignerオブジェクトを生成するサンプルコード
+
+```ts
+ // ethers.js をインポート
+ const ethers = require('ethers')
+ 
+ // 秘密鍵
+ let privateKey = "0x9729e15de7c9c0ec06ebc2ab7f4dcf796f24d5add48ddf3c424a8019e9061ad8"
+ 
+ // 秘密鍵からウォレットのインスタンスを作成
+ let wallet = new ethers.Wallet(privateKey)
+ 
+ // ウォレットのアドレスを取得
+ let address = wallet.address
+ console.log("address:", address)
+ // => 0xfC12b50bD2D04d3754BfC1cFB6c303fb9EAcA118
+```
+
+#### メモ 
+ゼロ知識証明を活かした鍵管理の仕組みはどうか？？
+
 ### 参考文献
 1. [blockchain-ts](https://github.com/mashharuki/blockchain-ts?organization=mashharuki&organization=mashharuki)
 2. [シャミアの秘密分散による マルチシグの実装](https://block-chain.jp/others/shamir-secret-sharing/)
 3. [【npm】shamirs-secret-sharing ](https://www.npmjs.com/package/shamirs-secret-sharing)
 4. [TypeScript + Node.js プロジェクトのはじめかた2020](https://qiita.com/notakaos/items/3bbd2293e2ff286d9f49)
+5. [Ethers Docs](https://docs.ethers.io/v5/)
--- sample/src/index.ts
+++ sample/src/index.ts
@@ -1,5 +1,44 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`;
+const sss = require('shamirs-secret-sharing');
+const fs = require('fs');
+const ethers = require('ethers');
+
+/**
+ * test code
+ */
+function test(): string {
+  // get Secret Key from file
+  const secret = fs.readFileSync('./sample/data/privatekey.txt')
+  // create shares
+  const shares = sss.split(secret, { shares: 3, threshold: 2 });
+  console.log("shares:", shares);
+
+  // output
+  for (var i=0; i < shares.length; i++ ) {
+    fs.writeFileSync(`./sample/data/shares${i+1}.txt`, shares[i], "utf-8");
   }
+
+  var shareDatas = [];
+
+  // get shares
+  const share1 = fs.readFileSync('./sample/data/shares1.txt');
+  const share2 = fs.readFileSync('./sample/data/shares2.txt');
+  const share3 = fs.readFileSync('./sample/data/shares3.txt');
+
+  shareDatas.push(share1);
+  shareDatas.push(share2);
+  shareDatas.push(share3);
+
+  console.log("shareDatas:", shareDatas)
+
+  // recovered by shares
+  const recovered = sss.combine(shareDatas.slice(1, 3));
+  console.log("recovered:", recovered)
+
+  // create instance from private
+  let wallet = new ethers.Wallet(recovered.toString());
+  // get wallet address
+  let address = wallet.address
+  return address;
+}
   
-console.log(hello("World"));
+console.log(test());
