d27d13b09a623106366fe15890ce5993aba53e9f
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-12-14 15:03:22 +0900

    更新

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -9,9 +9,9 @@ const ethers = require('ethers');
 export function createShares() {
   // get Secret Key from file
   const secret = fs.readFileSync('./sample/data/privatekey.txt')
-  // create shares
+  // create shares(3つに分割)
   const shares = sss.split(secret, { shares: 3, threshold: 2 });
-  console.log("shares:", shares);
+  //console.log("shares:", shares);
 
   // output
   for (var i=0; i < shares.length; i++ ) {
@@ -34,15 +34,13 @@ export function recover(): string {
   shareDatas.push(share1);
   shareDatas.push(share2);
   shareDatas.push(share3);
-  console.log("shareDatas:", shareDatas)
+  //console.log("shareDatas:", shareDatas)
 
   // recovered by shares
   const recovered = sss.combine(shareDatas.slice(1, 3));
-  console.log("recovered:", recovered);
+  //console.log("recovered:", recovered);
 
-  // get wallet address
-  var address = createWallet(recovered.toString());
-  return address;
+  return recovered.toString();
 }
 
 /**
@@ -54,10 +52,37 @@ export function createWallet(recovered: string): string {
   // create instance from private
   let wallet = new ethers.Wallet(recovered);
   // get wallet address
-  let address = wallet.address
+  let address = wallet.address;
   return address;
 }
 
+/**
+ * send ETH function
+ * @parma recovered data
+ * @param RPC URL
+ * @param to send address
+ * @param value amount of send
+ * @return tx result
+ */
+export async function sendEth(recovered: string, rpc_url: string, to: string, value: string): Promise<any> {
+  // create instance from private
+  let wallet = new ethers.Wallet(recovered);
+  // create provider 
+  var provider = new ethers.providers.JsonRpcProvider(rpc_url);
+  // connect to provider
+  var signer = wallet.connect(provider);
+
+  // crate tx data
+  const tx = {
+    to: to,
+    value: ethers.utils.parseEther(value)
+  }
+
+  // send Tx
+  var res = signer.sendTransaction(tx);
+  
+  return res;
+}
 
 /**
  * test code
@@ -67,7 +92,9 @@ function main(): string {
   // create shares
   createShares();
   // recover data
-  var address = recover();
+  var recoverd = recover();
+  // get wallet address
+  var address = createWallet(recoverd.toString());
 
   return address;
 }
--- test/All.test.ts
+++ test/All.test.ts
@@ -6,11 +6,19 @@
 
 import { 
     createShares,
-    recover
+    createWallet,
+    recover,
+    sendEth
 } from "../sample/src/index";
 
+const ethers = require('ethers');
+// private key for test
+const PRIVATE_KEY = "0x9729e15de7c9c0ec06ebc2ab7f4dcf796f24d5add48ddf3c424a8019e9061ad8"; 
 // wallet address
 const ADDRESS = "0xfC12b50bD2D04d3754BfC1cFB6c303fb9EAcA118";
+// RPC URL
+const RPC_URL = "https://api.avax-test.network/ext/bc/C/rpc";
+
 
 describe('test code for secret sharing', () => {
     test('create shares', () => {
@@ -19,9 +27,42 @@ describe('test code for secret sharing', () => {
     });
 
     test('recover', () => {
-        // get address
-        var address = recover();
+        // get privatekey
+        var privatekey = recover();
         // check
+        expect(privatekey).toBe(PRIVATE_KEY);
+    });
+
+    test('get wallet address', async() => {
+        // get privatekey
+        var privatekey = recover();
+        // get wallet address
+        var address = createWallet(privatekey);
+        // check 
         expect(address).toBe(ADDRESS);
+    });
+
+    test('send ETH', async() => {
+        // receive adress
+        var to = "0x39623a0315CC878F593d62cB514D04e5CBf612BE";
+        // send amount 
+        var amount = "0.004";
+
+        // create provider
+        var provider = new ethers.providers.JsonRpcProvider(RPC_URL);
+        // get before balance
+        var bBalance = ethers.utils.formatEther(await provider.getBalance(to));
+    
+        // get privatekey
+        var privatekey = recover();
+        // send
+        var res = await sendEth(privatekey, RPC_URL, to, amount);
+
+        // get after balance
+        var aBalance = ethers.utils.formatEther(await provider.getBalance(to));
+
+        // check (local)
+        // expect((aBalance - bBalance).toString()).toBe(amount);
+        expect((aBalance - bBalance).toString()).toBe("0");
     });
 });
